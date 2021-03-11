785a2a9830e6d48c80809463795366930c826135
Anton <AGlomadov@stud.kantiana.ru>
2021-03-11 19:50:51 +0200

    worker then last commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+using namespace std;
+
+int items[] = { -7, -3, -2,1, 5, 8 }; //Исходное множество
+int n = sizeof(items) / sizeof(int); //Размер
+int *set; //Искомое подмножество
+
+void subsum(int i, int weight) { //Поиск и вывод элементов
+    int j;
+    if (i && !weight) {
+        for (j = 0; j < i; j++) {
+            cout << " " << items[set[j]];
+        }
+        cout << endl;
+    }
+    for (j = i ? set[i - 1] + 1 : 0; j < n; j++) {
+        set[i] = j;
+        subsum(i + 1, weight + items[j]);
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    set = new int[n];
+    subsum(0, 0);
+    delete set;
+    cin.get();
     return 0;
 }
